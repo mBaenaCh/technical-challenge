d62ea35aae3e131b2a141877985084348730a280
mBaenaCh <mateo.baena@hotmail.com>
2021-09-19 18:10:39 -0500

    Creating banks of questions

--- src/app.py
+++ src/app.py
@@ -6,12 +6,17 @@ from string import ascii_lowercase, digits
 from random import choice
 import random
 
+ANSWERS_PER_QUESTIONS=4
+MIN_NUMBER_OF_CATEGORIES = 5
+MIN_NUMBER_OF_QUESTIONS = 25
+NUMBER_OF_ROUNDS = 5
+
+QUESTIONS = []
 player_1 = Player(1, "Mateo", 5, True)
 
 # Creating answers given a list of texts
 def creating_answers(answers_text, correct):
-    answers_per_question = 4
-    if len(answers_text) == answers_per_question and 0 < correct < len(answers_text):
+    if len(answers_text) == ANSWERS_PER_QUESTIONS and 0 < correct < len(answers_text):
         answers_list = []   
         random_ids = random.sample(range(101, 200), len(answers_text))
 
@@ -25,13 +30,13 @@ def creating_answers(answers_text, correct):
             print(i , answers_list[i])
         return answers_list
     else:
-        msg = "There must be "+ answers_per_question +"answers per question"
+        msg = "There must be "+ ANSWERS_PER_QUESTIONS +"answers per question"
         return msg
     
 # Creation of categories given a list of names 
 def setting_categories(categories_names):
-    number_of_categories = 5
-    if (len(categories_names) == number_of_categories):
+
+    if (len(categories_names) == MIN_NUMBER_OF_CATEGORIES):
         dificulty_level = 1
         random_ids = random.sample(range(1,100), len(categories_names))
         categories_list = []
@@ -44,15 +49,33 @@ def setting_categories(categories_names):
     
         return categories_list
     else:
-        msg = "You can only have "+ str(number_of_categories) +" categories"
+        msg = "You can only have "+ str(MIN_NUMBER_OF_CATEGORIES) +" categories"
         return msg
 
-# Creation of questions given a list of titles
-def creating_question_per_category(id, question_title, category, answers):
-    answers_per_questions = 4
-    if(len(answers) == 4):
-        question = Question(id, question_title, category, answers)
+# Creation of questions given a title, category and a list of answers
+def creating_question_per_category(id, question_title, category):
+    if(len(answers) == ANSWERS_PER_QUESTIONS):
+        question = Question(id, question_title, category, [])
         return question
     else:
-        msg = "Must have 4 answers per questions"
+        msg = "Must have " +str(ANSWERS_PER_QUESTIONS)+ " answers per questions"
         return msg
+
+# Asignation of a question to a bank given a category
+def creating_bank_of_questions_per_category(question, categories):
+    some_id = question.category_id
+    bank = []
+    for i in range(len(categories)):
+        if(some_id == categories[i].id):
+            bank.append(question)
+
+    return bank
+
+def assing_answers_to_question(question, answers):
+    question.answer_list = answers
+
+
+
+
+
+
--- src/question.py
+++ src/question.py
@@ -1,8 +1,8 @@
 class Question:
-    def __init__(self, id, title, categoryId, answer_list):
+    def __init__(self, id, title, category_id, answer_list):
         self.id = id
         self.title = title
-        self.categoryId = categoryId
+        self.category_id = category_id
         self.answer_list = answer_list
 
     @property
@@ -14,12 +14,12 @@ class Question:
         self._title = title
 
     @property
-    def categoryId(self):
-        return self._categoryId
+    def category_id(self):
+        return self._category_id
 
-    @categoryId.setter
-    def categoryId(self, categoryId):
-        self._categoryId = categoryId
+    @category_id.setter
+    def category_id(self, category_id):
+        self._category_id = category_id
 
     @property
     def answer_list(self):
@@ -30,4 +30,4 @@ class Question:
         self._answer_list = answer_list
 
     def __str__(self):
-        return str(self.id)+ " " + self.title + " " + str(self.categoryId)
+        return str(self.id)+ " " + self.title + " " + str(self.category_id)
