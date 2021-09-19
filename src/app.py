be940557d779f0dede59688f78e64808078c2d02
mBaenaCh <mateo.baena@hotmail.com>
2021-09-19 12:36:24 -0500

    Refactoring category configuration

--- src/app.py
+++ src/app.py
@@ -7,13 +7,13 @@ player_1 = Player(1, "Mateo", 5, True)
 
 # Creating answers given the text of them 
 def creating_answers(answers_text, correct):
-    if len(answers_text) == 4 and 0 < correct < len(answers_text):
+    answers_per_question = 4
+    if len(answers_text) == answers_per_question and 0 < correct < len(answers_text):
         answers_list = []   
-        random_ids = random.sample(range(1, 100), len(answers_text))
+        random_ids = random.sample(range(101, 200), len(answers_text))
 
         for i in range(len(answers_text)):
             if i == correct-1:
-
                 answer = Answer(random_ids[i], answers_text[i], True)
             else: 
                 answer = Answer(random_ids[i], answers_text[i], False)
@@ -22,26 +22,26 @@ def creating_answers(answers_text, correct):
             print(i , answers_list[i])
         return answers_list
     else:
-        msg = "There must be 4 answers per question"
+        msg = "There must be "+ answers_per_question +"answers per question"
         return msg
+    
 # Creation of categories given a number and names
-#RECIBIR SOLO EL NOMBRE DE LAS CATEGORIAS Y TRABAJAR CON EL LEN() DE ESE categories_name
-def setting_categories(categories_number, categories_name):
-    if (categories_number == len(categories_name)):
+def setting_categories(categories_names):
+    number_of_categories = 5
+    if (len(categories_names) == number_of_categories):
         dificulty_level = 1
-        random_ids = random.sample(range(1,100), categories_number)
+        random_ids = random.sample(range(1,100), len(categories_names))
         categories_list = []
 
         for i in range(len(random_ids)):
-            category = Category(random_ids[i], dificulty_level, categories_name[i])
-            print(category)
+            category = Category(random_ids[i], dificulty_level, categories_names[i])
+            print(i, category)
             categories_list.append(category)
             dificulty_level+=1
     
         return categories_list
     else:
-        msg = "You must give the same amount of names as categories"
+        msg = "You can only have "+ str(number_of_categories) +" categories"
         return msg
-
 
 
